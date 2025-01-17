f69da1628efccc6f7772b75cbb201b83014e6630
Pham Trung Hieu <trunghieu.pham1@Hieudeptrainhatvutru.local>
2025-01-17 17:46:26 +0700

    add extension

--- src/app/documents/[documentId]/Editor.tsx
+++ src/app/documents/[documentId]/Editor.tsx
@@ -2,6 +2,14 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
 
 export const Editor = () => {
   const editor = useEditor({
@@ -11,8 +19,26 @@ export const Editor = () => {
         class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
       }
     },
-    extensions: [StarterKit],
-    content: '<p>Hello World! 🌎️</p>',
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+      ImageResize
+    ],
+    content: `
+      <p>This is a basic example of implementing images. Drag to re-order.</p>
+      <img src="https://placehold.co/800x400" />
+      <img src="https://placehold.co/800x400/6A00F5/white" />
+    `,
   })
 
   return (
