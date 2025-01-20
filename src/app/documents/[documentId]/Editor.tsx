f536f4820ff92c921acc01abf61a87868ad83026
Pham Trung Hieu <trunghieu.pham1@Hieudeptrainhatvutru.local>
2025-01-20 11:55:55 +0700

    config store

--- src/app/documents/[documentId]/Editor.tsx
+++ src/app/documents/[documentId]/Editor.tsx
@@ -11,8 +11,33 @@ import TableRow from '@tiptap/extension-table-row'
 import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
 
+import { useEditorStore } from "@/store/use-editor-store"
+
 export const Editor = () => {
+  const { setEditor } = useEditorStore()
+
   const editor = useEditor({
+    onCreate({editor}) {
+      setEditor(editor)
+    },
+    onDestroy() {
+      setEditor(null)
+    },
+    onSelectionUpdate({editor}) {
+      setEditor(editor)
+    },
+    onTransaction({editor}) {
+      setEditor(editor)
+    },
+    onFocus({editor}) {
+      setEditor(editor)
+    },
+    onBlur({editor}) {
+      setEditor(editor)
+    },
+    onContentError({editor}) {
+      setEditor(editor)
+    },
     editorProps: {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px",
