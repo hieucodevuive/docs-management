3109fe8a096c558242759653dea7873b0c3f3a70
Pham Trung Hieu <trunghieu.pham1@Hieudeptrainhatvutru.local>
2025-01-20 16:51:40 +0700

    done heading

--- src/app/documents/[documentId]/Toolbar.tsx
+++ src/app/documents/[documentId]/Toolbar.tsx
@@ -22,6 +22,69 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { type Level } from "@tiptap/extension-heading"
+
+const HeadignLevelButton = () => {
+  const { editor } = useEditorStore()
+
+  const headings = [
+    { label: "Normal text", value: 0, fontSize: "16px"},
+    { label: "Heading 1", value: 1, fontSize: "32px"},
+    { label: "Heading 2", value: 2, fontSize: "24px"},
+    { label: "Heading 3", value: 3, fontSize: "20px"},
+    { label: "Heading 4", value: 4, fontSize: "18px"},
+    { label: "Heading 5", value: 5, fontSize: "16px"},
+  ]
+
+  const getCurrentHeading = () => {
+    for(let level = 1; level <= 5; level++ ) {
+      if (editor?.isActive("heading", { level })) {
+        return `Heading ${level}`
+      }
+    }
+    return "Normal text"
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button
+          className={cn(
+            "h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm",
+          )}
+        >
+          <span className="truncate">
+            {getCurrentHeading()}
+          </span>
+          <ChevronDownIcon className="ml-2 size-4 shrink-0"/>
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {
+          headings.map(({label, value, fontSize}) => (
+            <button
+              key={value}
+              style={{fontSize}}
+              className={cn(
+                "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                (value === 0 && !editor?.isActive("heading") || editor?.isActive("heading", {level: value})) && "bg-neutral-200/80"
+              )}
+              onClick={() => {
+                if (value === 0) {
+                  editor?.chain().focus().setParagraph().run()
+                } else {
+                  editor?.chain().focus().toggleHeading({level: value as Level}).run()
+                }
+              }}
+            >
+              <span>{label}</span>
+            </button>
+          ))
+        }
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 
 const FontFamilyButton = () => {
   const { editor } = useEditorStore()
@@ -203,7 +266,7 @@ const Toolbar = () => {
       <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
         <FontFamilyButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
-      {/* font heading */}
+        <HeadignLevelButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
       {/* font size */}
       <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
