5c8ee224e6d773bf51378249efbd20a13b3d78c9
Pham Trung Hieu <trunghieu.pham1@Hieudeptrainhatvutru.local>
2025-03-04 16:04:03 +0700

    setup data base

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,12 @@
-import Link from "next/link"
+"use client"
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 import Navbar from "./Navbar";
 import TemplateGallery from "./TemplateGallery";
 
 const Home = () => {
+  const documents = useQuery(api.documents.getDocuments)
+  console.log("🚀 ~ Home ~ documents:", documents)
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
